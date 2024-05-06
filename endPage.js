b001c19623dcbe4f660f4c3e491a567995a96907
Rahul-Chandravanshi <156426722+Rahul-Chandravanshi@users.noreply.github.com>
2024-05-06 11:41:08 +0530

    Update endPage.js

--- endPage.js
+++ endPage.js
@@ -1,5 +1,4 @@
 let endPage = document.createElement('div');
-//console.log('endPage is alive');
 endPage.classList.add('end-page');
 let endBox = document.createElement('div');
 endBox.classList.add('end-box');
@@ -39,18 +38,11 @@ window.location.href = 'https://rahul-chandravanshi.github.io/Free/'
 let popUp = document.createElement('div');
 popUp.classList.add('pop-up');
 
-setTimeout(function () {
-	endBox.appendChild(popUp);
-	typeWriter();
-}, 8500);
-
 
 let character = document.createElement('img');
 character.src = 'character.png';
 character.classList.add('character');
 popUp.appendChild(character);
-
-
 
 let popUpText = document.createElement('p');
 popUpText.innerHTML = 'I hope you appreciate our work. <br>';
@@ -58,23 +50,25 @@ popUpText.classList.add('popUp-text');
 popUp.appendChild(popUpText);
 
 function showEnd() {
-	
-	console.log('function is working');
 	document.body.appendChild(endPage);
+	setTimeout(function () {
+	endBox.appendChild(popUp);
+}, 5000);
+	setTimeout(function () {
+	typeWriter();
+}, 8000);
+	
 	console.log('Test complete');
-	
-	
-
 };
 
 let instagram = document.createElement('a');
 instagram.href = 'https://www.instagram.com/rahul.chandravanshi_0';
-instagram.classList.add('instaGram')
+instagram.classList.add('igBtn')
 instagram.textContent = 'Instagram';
 
 let youTube = document.createElement('a');
 youTube.href = 'https://youtube.com/@Rahul.chandravanshi_0';
-youTube.classList.add('youtube')
+youTube.classList.add('ytBtn')
 youTube.textContent = 'YouTube';
 
 popUp.appendChild(instagram);
@@ -88,8 +82,6 @@ function typeWriter() {
   if(index < text.length){
     popUpText.innerHTML += text.charAt(index);
     index++;
-    setTimeout(typeWriter, 200); // Adjust typing speed here
+    setTimeout(typeWriter, 200);
   }
 }
-
-
